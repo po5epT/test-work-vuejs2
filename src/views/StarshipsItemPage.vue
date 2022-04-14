bc929eda84084b265b51587141fe4adf89d823d5
po5epT <megastorm87@mail.ru>
2022-04-14 11:04:51 +0300

    fix

--- src/views/StarshipsItemPage.vue
+++ src/views/StarshipsItemPage.vue
@@ -1,43 +1,43 @@
 <template>
-    <div class="starship">
-        <div v-if="isLoading">Loading...</div>
-        <pre v-else>{{ starship }}</pre>
-    </div>
+  <div class="starship">
+    <div v-if="isLoading">Loading...</div>
+    <pre v-else>{{ starship }}</pre>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
-import {apiEndPoint} from '@/config';
+import { apiEndPoint } from '@/config';
 
 export default {
-    name: 'StarshipsItemPage',
-    props: {
-        id: {
-            type: [Number, String],
-            required: true,
-        }
+  name: 'StarshipsItemPage',
+  props: {
+    id: {
+      type: [Number, String],
+      required: true,
     },
-    data() {
-        return {
-            isLoading: false,
-            starship: null,
-        }
+  },
+  data() {
+    return {
+      isLoading: false,
+      starship: null,
+    };
+  },
+  methods: {
+    async fetchStarship(id) {
+      try {
+        this.isLoading = true;
+        const response = await axios.get(apiEndPoint + '/' + id);
+        this.starship = response.data;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isLoading = false;
+      }
     },
-    methods: {
-        async fetchStarship(id) {
-            try {
-                this.isLoading = true;
-                const response = await axios.get(apiEndPoint + '/' + id);
-                this.starship = response.data;
-            } catch (error) {
-                console.log(error);
-            } finally {
-                this.isLoading = false;
-            }
-        }
-    },
-    created() {
-        this.fetchStarship(this.id);
-    }
+  },
+  created() {
+    this.fetchStarship(this.id);
+  },
 };
 </script>
--- src/views/StartshipsListPage.vue
+++ src/views/StartshipsListPage.vue
@@ -1,109 +1,127 @@
 <template>
-    <div class="starships">
-        <h1>Starships</h1>
+  <div class="starships">
+    <h1>Starships</h1>
 
-        <input type="text" v-model="search" placeholder="Name of starship">
+    <input
+      v-model="search"
+      type="text"
+      placeholder="Name of starship"
+    >
 
-        <div>
-            <button @click="getPrev" :disabled="!prev">Prev</button>
-            <button @click="getNext" :disabled="!next">Next</button>
-        </div>
+    <div>
+      <button
+        @click="getPrev"
+        :disabled="!prev"
+      >
+        Prev
+      </button>
 
-        <div v-if="isLoading">Loading...</div>
-
-        <ul v-else>
-            <li v-for="starship in starships" :key="starship.id">
-                <router-link :to="{name: 'StarshipsItemPage', params: {id: starship.id}}">{{ starship.name }}</router-link>
-            </li>
-        </ul>
+      <button
+        @click="getNext"
+        :disabled="!next"
+      >
+        Next
+      </button>
     </div>
+
+    <div v-if="isLoading">Loading...</div>
+
+    <ul v-else>
+      <li
+        v-for="starship in starships"
+        :key="starship.id"
+      >
+        <router-link :to="{
+          name: 'StarshipsItemPage',
+          params: { id: starship.id }
+        }"
+        >
+          {{ starship.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 import axios from 'axios';
-import {throttle} from 'lodash';
-import {apiEndPoint, apiRequestDelay} from '@/config';
+import { throttle } from 'lodash';
+import { apiEndPoint, apiRequestDelay } from '@/config';
 
 export default {
-    name: 'StarshipsListPage',
-    data() {
-        return {
-            isLoading: false,
-            starships: [],
+  name: 'StarshipsListPage',
+  data() {
+    return {
+      isLoading: false,
+      starships: [],
 
-            search: this.$route.query.search || '',
+      search: this.$route.query.search || '',
 
-            page: 1,
-            next: null,
-            prev: null
-        };
+      page: 1,
+      next: null,
+      prev: null,
+    };
+  },
+  computed: {
+    params() {
+      return {
+        page: this.page,
+        search: this.search,
+      };
     },
-    computed: {
-        currentApiEndPoint() {
-            let url = apiEndPoint + '?';
+  },
 
-            if (this.page > 1) {
-                url += 'page=' + this.page + '&';
-            }
+  methods: {
+    fetchStarships: throttle(async function() {
+      try {
+        this.isLoading = true;
 
-            if (this.search) {
-                url += 'search=' + this.search;
-            }
+        const { data } = await axios.get(apiEndPoint, { params: this.params });
 
-            return url;
-        },
-    },
-    methods: {
-        async fetchStarships() {
-            try {
-                this.isLoading = true;
+        this.starships = data.results.map(starship => {
+          starship.id = starship.url
+            .replace(apiEndPoint, '')
+            .replaceAll('/', '');
 
-                const response = await axios.get(this.currentApiEndPoint);
-                this.starships = response.data.results.map(starship => {
-                    starship.id = starship.url
-                    .replace(apiEndPoint, '')
-                    .replaceAll('/', '');
+          return starship;
+        });
+        this.next = data.next;
+        this.prev = data.previous;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isLoading = false;
+      }
+    }, apiRequestDelay),
 
-                    return starship;
-                });
-                this.next = response.data.next;
-                this.prev = response.data.previous;
-            } catch (error) {
-                console.log(error);
-            } finally {
-                this.isLoading = false;
-            }
-        },
+    getPrev() {
+      if (!this.prev) {
+        return;
+      }
 
-        getPrev() {
-            if (!this.prev) {
-                return;
-            }
-
-            this.page--;
-            this.fetchStarships();
-        },
-
-        getNext() {
-            if (!this.next) {
-                return;
-            }
-
-            this.page++;
-            this.fetchStarships();
-        },
-    },
-    created() {
-        this.fetchStarships = throttle(this.fetchStarships, apiRequestDelay);
-        this.fetchStarships();
+      this.page--;
+      this.fetchStarships();
     },
 
-    watch: {
-        search() {
-            const query = this.search ? {search: this.search} : null;
+    getNext() {
+      if (!this.next) {
+        return;
+      }
 
-            this.$router.replace({query});
-            this.fetchStarships();
-        }
-    }
+      this.page++;
+      this.fetchStarships();
+    },
+  },
+  created() {
+    this.fetchStarships();
+  },
+
+  watch: {
+    search() {
+      const search = this.search ? this.search : null;
+
+      this.$router.replace({ query: { search } });
+      this.fetchStarships();
+    },
+  },
 };
 </script>
